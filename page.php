630b7d23b97d2ffa5d68ace29cc2625ea19ac17d
devonhg <devon.hugh.godfrey@gmail.com>
2015-12-29 10:26:58 -0500

    v1.0.3 Added events plugin support.

--- page.php
+++ page.php
@@ -16,9 +16,10 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 			<?php
+
 			while ( have_posts() ) : the_post();
+				the_post_thumbnail( 'large' );
 
 				get_template_part( 'template-parts/content', 'page' );
 
